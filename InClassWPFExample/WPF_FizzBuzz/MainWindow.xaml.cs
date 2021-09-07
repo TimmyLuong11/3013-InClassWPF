da648aa969e9beca107fcb4e9ec6a7230098027d
TimmyLuong11 <Timothy.luong@ou.edu>
2021-09-07 15:53:22 -0500

    Finished FizzBuzz

---
+++ InClassWPFExample/WPF_FizzBuzz/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_FizzBuzz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 1; i < 1001; i++)
+            {
+                if (i % 3 == 0 && i % 5 ==0)
+                {
+                    listBoxFizzBuzz.Items.Add(i);
+                }
+                else if(i % 5 == 0)
+                {
+                    listBoxBuzz.Items.Add(i);
+                }
+                else if(i % 3 == 0)
+                {
+                    listBoxFizz.Items.Add(i);
+                }
+            }
+        }
+    }
+}
