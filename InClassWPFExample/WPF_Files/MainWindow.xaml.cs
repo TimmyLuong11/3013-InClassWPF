cacb8d911ce8938c29f442f0b3ac2e76c37754fc
TimmyLuong11 <Timothy.luong@ou.edu>
2021-09-28 16:10:21 -0500

    Added JSON Example

---
+++ InClassWPFExample/WPF_Files/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Files
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRead_Click(object sender, RoutedEventArgs e)
+        {
+            string file = txtBox.Text;
+
+            string[] lines = File.ReadAllLines("flie");
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] pieces = line.Split(',');
+                listBox.Items.Add(pieces);
+            }
+        }
+    }
+}
